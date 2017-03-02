a08aeaae730b5ca544e98350cf174fde719cb270
Francis Bosse <francis.bosse@dal.ca>
2017-03-01 23:02:58 -0400

    Stage 1 - assignment 3

---
+++ assignment3/src/main/java/com/email_validator/part1/validator.java
@@ -0,0 +1,22 @@
+package com.email_validator.part1;
+
+
+public class validator
+{
+	public static int validate( String str) {
+		int count = 0;
+		if( atValid(str)) {
+			count++;
+		}
+		if( pointValid(str)) {
+			count++;
+		}
+		return count;
+	}
+	public static boolean atValid( String str) {
+		return str.matches("[^@}@[^@]");
+	}
+	public static boolean pointValid( String str) {
+		return str.matches("*\\.*");
+	}
+}
